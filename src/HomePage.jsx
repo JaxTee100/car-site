183eb84c5a5690058dfdf4e22295a3fae5269f4f
Tobias <redefineddevelopertee@gmail.com>
2023-09-17 23:54:55 +0100

    made a recent update

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is the home Page</h1>
+      <Link to='/honda-home'>Click me</Link>
     </div>
   )
 }
